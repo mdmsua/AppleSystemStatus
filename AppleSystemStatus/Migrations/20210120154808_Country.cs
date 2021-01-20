529f0abcb4e7de47e82430b2d0397a4789b41105
Dmytro Morozov <mdmsua@icloud.com>
2021-01-20 21:51:34 +0100

    Provision canary infrastructure and database user

---
+++ AppleSystemStatus/Migrations/20210120154808_Country.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AppleSystemStatus.Migrations
+{
+    public partial class Country : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Services",
+                maxLength: 96,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(450)");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Services",
+                type: "nvarchar(450)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldMaxLength: 96);
+        }
+    }
+}
