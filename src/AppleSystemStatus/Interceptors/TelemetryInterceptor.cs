a4ad3a2726d74cc5379cb579c679c2db2db8270f
Dmytro Morozov <mdmsua@icloud.com>
2021-03-09 10:43:14 +0100

    Change dependency name in telemetry interceptor

--- src/AppleSystemStatus/Interceptors/TelemetryInterceptor.cs
+++ src/AppleSystemStatus/Interceptors/TelemetryInterceptor.cs
@@ -19,54 +19,54 @@ namespace AppleSystemStatus.Interceptors
 
         public override DbCommand CommandCreated(CommandEndEventData eventData, DbCommand result)
         {
-            telemetryClient.TrackDependency(dependencyType, eventData.CommandId.ToString(), eventData.Command.CommandText, eventData.StartTime, eventData.Duration, true);
+            telemetryClient.TrackDependency(dependencyType, eventData.Connection.DataSource, eventData.Command.CommandText, eventData.StartTime, eventData.Duration, true);
             return result;
         }
 
         public override void CommandFailed(DbCommand command, CommandErrorEventData eventData)
         {
-            telemetryClient.TrackDependency(dependencyType, eventData.ExecuteMethod.ToString(), eventData.Command.CommandText, eventData.StartTime, eventData.Duration, false);
+            telemetryClient.TrackDependency(dependencyType, eventData.Connection.DataSource, eventData.Command.CommandText, eventData.StartTime, eventData.Duration, false);
         }
 
         public override Task CommandFailedAsync(DbCommand command, CommandErrorEventData eventData, CancellationToken cancellationToken = default)
         {
-            telemetryClient.TrackDependency(dependencyType, eventData.ExecuteMethod.ToString(), eventData.Command.CommandText, eventData.StartTime, eventData.Duration, false);
+            telemetryClient.TrackDependency(dependencyType, eventData.Connection.DataSource, eventData.Command.CommandText, eventData.StartTime, eventData.Duration, false);
             return Task.CompletedTask;
         }
 
         public override int NonQueryExecuted(DbCommand command, CommandExecutedEventData eventData, int result)
         {
-            telemetryClient.TrackDependency(dependencyType, eventData.CommandId.ToString(), eventData.Command.CommandText, eventData.StartTime, eventData.Duration, true);
+            telemetryClient.TrackDependency(dependencyType, eventData.Connection.DataSource, eventData.Command.CommandText, eventData.StartTime, eventData.Duration, true);
             return result;
         }
 
         public override Task<int> NonQueryExecutedAsync(DbCommand command, CommandExecutedEventData eventData, int result, CancellationToken cancellationToken = default)
         {
-            telemetryClient.TrackDependency(dependencyType, eventData.CommandId.ToString(), eventData.Command.CommandText, eventData.StartTime, eventData.Duration, true);
+            telemetryClient.TrackDependency(dependencyType, eventData.Connection.DataSource, eventData.Command.CommandText, eventData.StartTime, eventData.Duration, true);
             return Task.FromResult(result);
         }
 
         public override DbDataReader ReaderExecuted(DbCommand command, CommandExecutedEventData eventData, DbDataReader result)
         {
-            telemetryClient.TrackDependency(dependencyType, eventData.CommandId.ToString(), eventData.Command.CommandText, eventData.StartTime, eventData.Duration, true);
+            telemetryClient.TrackDependency(dependencyType, eventData.Connection.DataSource, eventData.Command.CommandText, eventData.StartTime, eventData.Duration, true);
             return result;
         }
 
         public override Task<DbDataReader> ReaderExecutedAsync(DbCommand command, CommandExecutedEventData eventData, DbDataReader result, CancellationToken cancellationToken = default)
         {
-            telemetryClient.TrackDependency(dependencyType, eventData.CommandId.ToString(), eventData.Command.CommandText, eventData.StartTime, eventData.Duration, true);
+            telemetryClient.TrackDependency(dependencyType, eventData.Connection.DataSource, eventData.Command.CommandText, eventData.StartTime, eventData.Duration, true);
             return Task.FromResult(result);
         }
 
         public override object ScalarExecuted(DbCommand command, CommandExecutedEventData eventData, object result)
         {
-            telemetryClient.TrackDependency(dependencyType, eventData.CommandId.ToString(), eventData.Command.CommandText, eventData.StartTime, eventData.Duration, true);
+            telemetryClient.TrackDependency(dependencyType, eventData.Connection.DataSource, eventData.Command.CommandText, eventData.StartTime, eventData.Duration, true);
             return result;
         }
 
         public override Task<object> ScalarExecutedAsync(DbCommand command, CommandExecutedEventData eventData, object result, CancellationToken cancellationToken = default)
         {
-            telemetryClient.TrackDependency(dependencyType, eventData.CommandId.ToString(), eventData.Command.CommandText, eventData.StartTime, eventData.Duration, true);
+            telemetryClient.TrackDependency(dependencyType, eventData.Connection.DataSource, eventData.Command.CommandText, eventData.StartTime, eventData.Duration, true);
             return Task.FromResult(result);
         }
     }
